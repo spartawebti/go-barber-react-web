24444e6bf0e7aa10dc30142904551ff7a114981a
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-26 23:24:45 -0300

    Pagina Cadastro

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImg from '../../assets/sign-in-background.png';
+import background from '../../assets/cadastro.png';
 
 export const Container = styled.div`
     height: 100vh;
@@ -46,7 +46,7 @@ export const Content = styled.div`
         }
     }
     > a {
-        color: #ff9000;
+        color: #F4EDE8;
         display: block;
         margin-top: 24px;
         text-decoration: none;
@@ -56,7 +56,7 @@ export const Content = styled.div`
         align-items: center;
 
         &:hover {
-                color: ${shade(0.2, '#ff9000')};
+                color: ${shade(0.2, '#F4EDE8')};
             }
 
         svg{
@@ -66,6 +66,6 @@ export const Content = styled.div`
 `;
 export const Background = styled.div`
     flex: 1;
-    background: url(${signInBackgroundImg}) no-repeat center;
+    background: url(${background}) no-repeat center;
     background-size: cover;
 `;
